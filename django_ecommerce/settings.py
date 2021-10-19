fefdcd038185197a069b4cd764d9c00900d9d1b3
Amar-shrestha <shresthaamar12345@gmail.com>
2021-10-19 21:02:06 +0545

    modified store templates

--- django_ecommerce/settings.py
+++ django_ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f61%1+=f22uxd@h$epc6sgce^=zjj8k8)vqft@0ut#12j03=_)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'category.context_processors.menu_links',
             ],
         },
     },
