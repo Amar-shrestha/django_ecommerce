0ca1d09ef58f4aff93d609fa138606d67a00c158
Amar-shrestha <shresthaamar12345@gmail.com>
2021-12-14 23:10:35 +0545

    fixed handling error codes in cart views

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product
 from .models import Cart, CartItem
+from django.core.exceptions import ObjectDoesNotExist
 
 def _cart_id(request):
 	cart = request.session.session_key
@@ -67,7 +68,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
 		tax = (2 * total)/100
 		grand_total = total + tax
 
-	except objectNotExist:
+	except ObjectDoesNotExist:
 		pass
 
 	context = {
